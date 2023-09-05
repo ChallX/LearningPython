c399b9838f8583916f8220248f441be71be90af7
LouChall <louismarchal237@gmail.com>
2023-09-05 11:26:39 +0700

    New Things

---
+++ coba.py
@@ -0,0 +1,115 @@
+#x = 120
+#y = "Hai"
+
+#x = 5
+#print(type(x))
+
+#y = 0.5
+#print(type(y))
+
+#mobil = "Hijau"
+#print(type(mobil))
+
+#print ("Universitas " + "Pertamina")
+
+#print ("Universitas Pertamina " * 3)
+
+#print (5*2)
+#print (5*2.5)
+
+#(5*2) > (5*3)
+
+#x = 5
+#(x**2) + (x**3) + (x+2) - (x/2)
+
+#uang = 30000
+#if uang >= 25000:
+#  print("Uang Cukup")
+#else:
+#  print("Uang Tidak Cukup")
+
+#price = 25000
+#uang = 10000
+#if price == uang:
+#  print("Uang Pas")
+#elif price < 25000:
+#  print("Uang Lebih")
+#else:
+#  print("Uang Tidak Cukup")
+
+#nilai = 80
+#if nilai >= 90:
+#  print("A")
+#elif nilai >= 70 and nilai < 90:
+#  print("B")
+#elif nilai < 70:
+#  print("C")
+
+#price = 100000 
+#kondisi = 'jelek'
+#if price >= 25000:
+#  print("uang pas")
+#  if kondisi == 'baik':
+#    print("Uang Laku")
+#  else:
+#    print("Uang Tidak Laku")
+#else:
+#  print("Uang Tidak Cukup")
+
+#for i in range(10):
+ # print("Saya berjanji tidak akan mengulanginya lagi")
+
+#j = 0
+#while j<10:
+#  print (j)
+#  j = j + 1
+ 
+#total = 0 
+#for i in range(1,1000):
+#     total = total + i
+#print (total) 
+
+#for i in range (1,999,3):
+#    print (i, end =" ")
+
+#for i in range(1,6):
+#    for j in range(1,3):
+#        print("Ini Lorong Ke- " , i , "Kamar Ke- " , j)
+
+#lanjut = True 
+#while(lanjut):
+#    option = input("Lanjut atau tidak? (y/n)")
+#    if(option == 'y'):
+#        lanjut = True
+#        print("Program Jalan!")
+#    elif(option == 'n'):
+#        lanjut = False
+#        print("Program Berhenti")
+        
+#username = "Universitas"
+#password = "Pertamina"
+#kesempatan = 3
+
+#while (kesempatan > 0):
+#    username_user = input("Masukkan username")
+#    password_user = input("Masukkan Password")
+    
+#    if(username == username_user and password == password_user):
+#        print("Login Berhasil")
+#        break
+#    else:
+#       kesempatan -= 1
+#        print("Kesempatan tersisa", {format(kesempatan)} , " kali lagi"  )
+
+#def perkalian (angka1, angka2):
+#    return angka1 + angka2
+
+#hasil = perkalian (10,2)
+#print(hasil)
+
+
+#def f (angka):
+#   return (angka**2) + (angka**3) + (angka+2) - (angka/2)
+
+#hasil = f(3)
+#print(hasil)
