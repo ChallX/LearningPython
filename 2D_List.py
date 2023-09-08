2c6d790b99ad83c591e5d80dfaff62c9c4064701
LouChall <louismarchal237@gmail.com>
2023-09-08 10:38:32 +0700

    Task

---
+++ 2D_List.py
@@ -0,0 +1,9 @@
+
+
+drinks = ["Coca - Cola", "Pepsi", "Water"]
+food = ["Pizza", "Noodle", "Fried Rice"]
+dessert = ["Kue", "Pudding", "Jelly"]
+
+menu = (drinks, food, dessert)
+
+print(menu[1][2])
---
+++ list.py
@@ -0,0 +1,14 @@
+
+food = ["Pisang", "Hamburger", "Salak", "Molen"]
+
+food[1] = "Asep" #Digunakan Untuk Mengganti dan menampilkan Suatu Index
+
+print(food[1])
+# food.append("Ice Cream") #Digunakan Untuk Menambah Variabel/Pesan Ke List
+# food.remove("Asep") #Digunakan untuk menghapus suatu index/variable di list
+# food.pop() #Digunakan Untuk Menghapus Index/element terakhir di list
+#food.insert(1,"Kue") #Digunakan Untuk Mengganti Sebuah Index Di List
+#food.sort() #Digunakan Untuk Mengurutkan List Sesuai Alphabet
+#food.clear() #Digunakan Untuk Menghilangkan Isi List
+for x in food:
+    print (x)
